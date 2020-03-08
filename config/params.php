68fc7390c18c0cd4bed406136146b45c7c7bdbde
romoc <romochka-remnev@yandex.ru>
2020-03-08 04:00:12 +0300

    blog finish2

--- config/params.php
+++ config/params.php
@@ -5,5 +5,5 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'dir_tmpl' => "/views/site/",
-    'insite' => "http://parabellum.ru",
+    'insite' => "http://para.ru",
 ];
